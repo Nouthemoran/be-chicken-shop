fd62306875699e3026131f6a53a44e1f1e0262fe
Nouthemoran <mardiansahafiz@gmail.com>
2024-02-16 19:44:21 +0700

    update index

--- .history/index_20240216193625.js
+++ .history/index_20240216193625.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import CustomerRoute from "./routes/CustomerRoute.js";
+// import CustomerRoute from "./routes/SppRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(CustomerRoute);
+// app.use(CustomerRoute);
 
 
 app.listen(5000, () => console.log('Server jalan..'));
--- .history/index_20240216193637.js
+++ .history/index_20240216193637.js
@@ -5,7 +5,7 @@ import CustomerRoute from "./routes/CustomerRoute.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(CustomerRoute);
+// app.use(CustomerRoute);
 
 
 app.listen(5000, () => console.log('Server jalan..'));
